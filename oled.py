83cbcffaac24595ff2c91013a147ed870cbdc5ae
PatrykDu <patrykdudka9@gmail.com>
2026-06-14 21:17:52 +0200

    test

--- oled.py
+++ oled.py
@@ -3,11 +3,17 @@ import busio
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_ssd1306
 
-i2c = busio.I2C(board.SCL, board.SDA)
-oled = adafruit_ssd1306.SSD1306_I2C(64, 48, i2c)
+WIDTH = 64
+HEIGHT = 48
 
-def display(n):
-    text = str(n)
+VISIBLE_Y = 10
+VISIBLE_H = HEIGHT - VISIBLE_Y
+
+i2c = busio.I2C(board.SCL, board.SDA)
+oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c)
+
+def display(value):
+    text = str(value)
 
     for size in range(200, 5, -1):
         font = ImageFont.truetype(
@@ -15,22 +21,21 @@ def display(n):
             size
         )
 
-        image = Image.new("1", (64, 48))
+        image = Image.new("1", (WIDTH, HEIGHT))
         draw = ImageDraw.Draw(image)
-
         bbox = draw.textbbox((0, 0), text, font=font)
 
         w = bbox[2] - bbox[0]
         h = bbox[3] - bbox[1]
 
-        if w <= 64 and h <= 48:
+        if w <= WIDTH and h <= VISIBLE_H:
             break
 
-    image = Image.new("1", (64, 48))
+    image = Image.new("1", (WIDTH, HEIGHT))
     draw = ImageDraw.Draw(image)
 
-    x = (64 - w) // 2 - bbox[0]
-    y = (48 - h) // 2 - bbox[1]
+    x = (WIDTH - w) // 2 - bbox[0]
+    y = VISIBLE_Y + (VISIBLE_H - h) // 2 - bbox[1]
 
     draw.text((x, y), text, font=font, fill=255)
 
@@ -38,4 +43,4 @@ def display(n):
     oled.show()
 
 
-display("8")
+display("P")
