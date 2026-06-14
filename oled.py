0d347dc2870fde180d8ed038ce552e9ea98cd9d1
PatrykDu <patrykdudka9@gmail.com>
2026-06-14 13:28:06 +0200

    updated oled.py

--- oled.py
+++ oled.py
@@ -1,15 +1,17 @@
 from luma.core.interface.serial import i2c
-from luma.oled.device import ssd1306
+from luma.oled.device import sh1106
 from PIL import Image, ImageDraw
 
 serial = i2c(port=1, address=0x3C)
-device = ssd1306(serial, width=64, height=48)
+device = sh1106(serial, width=64, height=48, rotate=0)
 
-image = Image.new("1", (64, 48))
+device.clear()
+
+image = Image.new("1", (64, 48), 0)
 draw = ImageDraw.Draw(image)
 
-draw.text((0, 0), "Hello")
-draw.text((0, 16), "Patryk")
-draw.text((0, 32), "RPi 5 OK")
+draw.rectangle((0, 0, 63, 47), outline=255)
+draw.text((4, 4), "HELLO", fill=255)
+draw.text((4, 20), "OLED OK", fill=255)
 
 device.display(image)
