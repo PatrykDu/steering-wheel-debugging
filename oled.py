4ce61440a458bddddaea6d7f5720890b37cf2e0e
PatrykDu <patrykdudka9@gmail.com>
2026-06-14 13:48:31 +0200

    test

--- oled.py
+++ oled.py
@@ -6,20 +6,31 @@ import adafruit_ssd1306
 i2c = busio.I2C(board.SCL, board.SDA)
 oled = adafruit_ssd1306.SSD1306_I2C(64, 48, i2c)
 
-font = ImageFont.truetype(
-    "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf",
-    64
-)
-
 def display(n):
+    text = str(n)
+
+    for size in range(200, 5, -1):
+        font = ImageFont.truetype(
+            "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf",
+            size
+        )
+
+        image = Image.new("1", (64, 48))
+        draw = ImageDraw.Draw(image)
+
+        bbox = draw.textbbox((0, 0), text, font=font)
+
+        w = bbox[2] - bbox[0]
+        h = bbox[3] - bbox[1]
+
+        if w <= 64 and h <= 48:
+            break
+
     image = Image.new("1", (64, 48))
     draw = ImageDraw.Draw(image)
 
-    text = str(n)
-    bbox = draw.textbbox((0, 0), text, font=font)
-
-    x = (64 - (bbox[2] - bbox[0])) // 2
-    y = (48 - (bbox[3] - bbox[1])) // 2
+    x = (64 - w) // 2 - bbox[0]
+    y = (48 - h) // 2 - bbox[1]
 
     draw.text((x, y), text, font=font, fill=255)
 
