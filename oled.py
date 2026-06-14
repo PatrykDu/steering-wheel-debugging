15f87a51c13e7c72f31262c2647547571c05f7d6
PatrykDu <patrykdudka9@gmail.com>
2026-06-14 13:43:37 +0200

    test

--- oled.py
+++ oled.py
@@ -3,31 +3,23 @@ import busio
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_ssd1306
 
-WIDTH = 64
-HEIGHT = 48
-
 i2c = busio.I2C(board.SCL, board.SDA)
-oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=0x3C)
+oled = adafruit_ssd1306.SSD1306_I2C(64, 48, i2c)
 
-def display(number: int):
-    if number not in range(1, 9):
-        raise ValueError("number must be 1..8")
+font = ImageFont.truetype(
+    "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf",
+    52
+)
 
-    image = Image.new("1", (WIDTH, HEIGHT), 0)
+def display(n):
+    image = Image.new("1", (64, 48))
     draw = ImageDraw.Draw(image)
 
-    font = ImageFont.truetype(
-        "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf",
-        52
-    )
-
-    text = str(number)
+    text = str(n)
     bbox = draw.textbbox((0, 0), text, font=font)
-    text_w = bbox[2] - bbox[0]
-    text_h = bbox[3] - bbox[1]
 
-    x = (WIDTH - text_w) // 2 - bbox[0]
-    y = (HEIGHT - text_h) // 2 - bbox[1]
+    x = (64 - (bbox[2] - bbox[0])) // 2
+    y = (48 - (bbox[3] - bbox[1])) // 2
 
     draw.text((x, y), text, font=font, fill=255)
 
@@ -35,5 +27,4 @@ def display(number: int):
     oled.show()
 
 
-# test
-display(9)
+display(1)
